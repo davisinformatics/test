57c20d125c62e6a2b11f6fb5b3675c6a8f27d25e
davisinformatics <32305067+davisinformatics@users.noreply.github.com>
2018-04-03 23:23:48 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+import os
+from collections import namedtuple
+namedtuple()
+[3, 2, 1].count(3)
+abs(-1)
+
+
+def hello_world(name):
+    print('hello', name)
+    print('how are you?')
+
+
+hello_world('Jerry')
